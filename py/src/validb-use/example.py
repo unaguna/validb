3114e198b826c06397bc15586eae85752e82d71f
K-izumi <k.izumi.ysk@gmail.com>
2024-02-25 14:05:42 +0900

    🚧 Rule.id_of_row

--- py/src/validb-use/example.py
+++ py/src/validb-use/example.py
@@ -22,14 +22,16 @@ class MyDetected(Detected[str, MyMsgType, str]):
         return f"<MyDetected: {self.row()}>"
 
 
-rules: t.List[SimpleRule[MyMsgType, str]] = [
+rules: t.List[SimpleRule[str, MyMsgType, str]] = [
     SimpleRule(
-        "SELECT * FROM country where InDepYear is NULL",
+        "SELECT Code FROM country where InDepYear is NULL",
+        lambda r: r[0],
         MyMsgType.NULL_YEAR,
         "null year",
     ),
     SimpleRule(
-        "SELECT * FROM country where SurfaceArea < Population",
+        "SELECT Code FROM country where SurfaceArea < Population",
+        lambda r: r[0],
         MyMsgType.TOO_SMALL,
         "too small",
     ),
@@ -48,9 +50,11 @@ if __name__ == "__main__":
     detection_data: DetectionData[str, MyMsgType, str] = DetectionData()
     for rule in rules:
         sql = text(rule.sql)
-        for r in session.execute(sql).mappings():
+        for r in session.execute(sql):
             msg_type, msg = rule.detected()
-            detection_data.append(MyDetected(id=r["Code"], msg_type=msg_type, msg=msg))
+            detection_data.append(
+                MyDetected(id=rule.id_of_row(r), msg_type=msg_type, msg=msg)
+            )
 
     for id in detection_data.ids():
         print(detection_data[id])
--- py/src/validb/_rule.py
+++ py/src/validb/_rule.py
@@ -1,12 +1,19 @@
 import abc
 import typing as t
-from ._detected import MSG, MSG_TYPE
+
+import sqlalchemy
+
+from ._detected import ID, MSG, MSG_TYPE
 
 
-class Rule(t.Generic[MSG_TYPE, MSG], abc.ABC):
+class Rule(t.Generic[ID, MSG_TYPE, MSG], abc.ABC):
     @property
     @abc.abstractmethod
     def sql(self) -> str:
+        pass
+
+    @abc.abstractmethod
+    def id_of_row(self, row: sqlalchemy.Row[t.Any]) -> ID:
         pass
 
     @abc.abstractmethod
@@ -14,21 +21,28 @@ class Rule(t.Generic[MSG_TYPE, MSG], abc.ABC):
         pass
 
 
-class SimpleRule(t.Generic[MSG_TYPE, MSG], Rule[MSG_TYPE, MSG]):
+class SimpleRule(t.Generic[ID, MSG_TYPE, MSG], Rule[ID, MSG_TYPE, MSG]):
     _sql: str
+    _id_of_row: t.Callable[[t.Any], ID]
     _msg_type: MSG_TYPE
     _msg: MSG
 
-    def __init__(self, sql: str, msg_type: MSG_TYPE, msg: MSG) -> None:
+    def __init__(
+        self, sql: str, id_of_row: t.Callable[[t.Any], ID], msg_type: MSG_TYPE, msg: MSG
+    ) -> None:
         super().__init__()
 
         self._sql = sql
+        self._id_of_row = id_of_row
         self._msg_type = msg_type
         self._msg = msg
 
     @property
     def sql(self) -> str:
         return self._sql
+
+    def id_of_row(self, row: t.Any) -> ID:
+        return self._id_of_row(row)
 
     def detected(self) -> t.Tuple[MSG_TYPE, MSG]:
         return self._msg_type, self._msg
