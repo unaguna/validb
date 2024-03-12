ad10a34b19e97a62f56bec7c21e141cfc6bd584c
K-izumi <k.izumi.ysk@gmail.com>
2024-03-12 19:45:26 +0900

    📝 docstring

--- py/src/validb/dtcsvmapping/_csvmapping.py
+++ py/src/validb/dtcsvmapping/_csvmapping.py
@@ -6,15 +6,51 @@ from .._detectiondata import DetectionData
 
 
 class DetectionCsvMapping(abc.ABC):
+    """mapper from detection to CSV row"""
+
     @abc.abstractmethod
-    def row(self, detected: Detected[t.Any, t.Any, t.Any]) -> t.Sequence[t.Any]: ...
+    def row(self, detected: Detected[t.Any, t.Any, t.Any]) -> t.Sequence[t.Any]:
+        """mapping from detection to CSV row
+
+        Parameters
+        ----------
+        detected : Detected
+            Anomalies detected
+
+        Returns
+        -------
+        t.Sequence
+            the row of CSV
+        """
+        ...
 
     def __call__(self, detected: Detected[t.Any, t.Any, t.Any]) -> t.Sequence[t.Any]:
+        """mapping from detection to CSV row
+
+        It is same to self.row().
+        """
         return self.row(detected)
 
     def rows(
         self, detection_data: DetectionData[t.Any, t.Any, t.Any]
     ) -> t.Iterator[t.Sequence[t.Any]]:
+        """map detections to CSV rows
+
+        It uses self.row() for each detected anomaly contained in the specified DetectionData.
+
+        The iterator returned by this function can be used directly for CSV output;
+        such as `csv_writer.writerows(csv_mapping.rows(detection_data))`.
+
+        Parameters
+        ----------
+        detection_data : DetectionData
+            a DetectionData, which contains detected anomalies
+
+        Returns
+        -------
+        t.Iterator[t.Sequence]
+            an iterator of the CSV rows
+        """
         return (self(detected) for detected in detection_data.values())
 
 
