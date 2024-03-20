9985c5e3886e8e91e0d98e6f7373550ca7a6a029
K-izumi <k.izumi.ysk@gmail.com>
2024-03-20 18:58:12 +0900

    ✨ stdout summary table

--- py/src/validb/__main__.py
+++ py/src/validb/__main__.py
@@ -3,7 +3,7 @@ import typing as t
 
 import click
 
-from validb import validate_db
+from validb import validate_db, DetectionData
 from validb.config import load_config
 from validb.csvmapping import SimpleDetectionCsvMapping
 
@@ -23,10 +23,14 @@ def main(config_path: str, dest_csv_path: t.Union[str, None]):
             embedders=config.embedders,
         )
 
-    click.echo(f"Detected: {detection_data.count}")
     if detection_data.count <= 0:
+        click.echo(f"No anomalies detected.")
         exit(0)
     else:
+        _output_summary(detection_data)
+        click.echo()
+        click.echo(f"Detected: {detection_data.count}")
+
         detected_csvmapping = (
             config.detected_csvmapping
             if config.detected_csvmapping is not None
@@ -39,6 +43,25 @@ def main(config_path: str, dest_csv_path: t.Union[str, None]):
                 csv_writer.writerows(detected_csvmapping.rows(detection_data))
 
         exit(10)
+
+
+def _output_summary(detection_data: DetectionData[str, str, str]):
+    title_row = ("DETECTION_TYPE", "COUNT")
+    rows = [
+        (detection_name, len(detection_data[detection_name]))
+        for detection_name in detection_data.detection_types()
+    ]
+
+    max_detection_type_len = max(len(title_row[0]), max(len(name) for name, _ in rows))
+    max_count_len = max(len(title_row[1]), max(len(str(count)) for _, count in rows))
+
+    for detection_type, count in (title_row, *rows):
+        click.echo(
+            "{}  {}".format(
+                format(detection_type, f"<{max_detection_type_len}"),
+                format(count, f">{max_count_len}"),
+            )
+        )
 
 
 if __name__ == "__main__":
